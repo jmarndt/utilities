32db34ba98f4e903e881a02a6f2f4b3f72858ddb
Joe Arndt <jmarndt@users.noreply.github.com>
2023-10-17 14:52:00 -0500

    updated docker install and debian init

--- debian/initialize_debian.sh
+++ debian/initialize_debian.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 apt update && apt upgrade -y
-apt install -y vim git ufw tree python3 python3-pip python3-venv ca-certificates curl gnupg
+apt install -y vim git curl gnupg ufw tree python3 python3-pip python3-venv
 
 echo "y" | ufw enable
 ufw default deny incoming
